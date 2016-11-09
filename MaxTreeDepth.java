b3bd8b87416b8e07a28154623b3a036318d9d2db
Deelons <deelons996@gmail.com>
2016-11-09 16:04:00 +0800

    11/9

---
+++ MaxTreeDepth.java
@@ -0,0 +1,24 @@
+import java.util.*;
+
+public class MaxTreeDepth {
+	public int maxDepth(TreeNode root) {
+		if (root == null) {
+			return 0;
+		}
+
+		int left = maxDepth(root.left);
+		int right = maxDepth(root.right);
+		return Math.max(left,right) + 1;
+	}
+
+}
+
+
+class TreeNode {
+	int val;
+	TreeNode left;
+	TreeNode right;
+	TreeNode(int val) {
+		this.val = val;
+	}
+}
